933be48c50bb438a11354880c496823c59623baa
HieuTV <hieutv@hybrid-technologies.co.jp>
2020-04-29 08:30:39 +0700

    test firebase

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,14 +1,14 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "33a29cfc7cbec781f03345b3dba6724c",
-"/": "33a29cfc7cbec781f03345b3dba6724c",
-"main.dart.js": "8a181f6ba7b58cc01ec6bf4a4ac0d987",
+  "index.html": "714ce5a49fca0d34173319f24f452865",
+"/": "714ce5a49fca0d34173319f24f452865",
+"main.dart.js": "54cb381bb0e72ec72e35b79a9a979b71",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "0812958397bca6955aa51947b59cb995",
-"assets/LICENSE": "15e3b196ed5d94f4a41d0304f6b8a680",
+"assets/LICENSE": "b09eb7ff295f5bc65b5d6392666f98e3",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
